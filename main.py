fe54097b4fd689cd2e715317eea7e30fa2d08b43
KhilfiKhairulAmin <khilfikhairulamin@gmail.com>
2024-04-12 18:17:58 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+"""
+Program: our_project.py
+Author: Khilfi, Murfiqah, Zainatul
+No description.
+"""
+
+"""
+PROJECT REQUIREMENTS
+A) FEATURE FOR CUSTOMERS
+  - Display list of food items for 3 menu categories:
+    1. Entree
+    2. Side Dish
+    3. Drink
+    (refer table for all items information)
+  - Calculate total cost of order + 10% tax
+  - Rate foods
+  - Pick order from prompter box
+
+B) FEATURE FOR RESTAURANT MANAGER
+  - Add new food items and its prices
+
+
+ADDITIONAL FEATURES FOR OUR PROJECT
+To be confirmed.
+"""
