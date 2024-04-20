835c86a91d80e2a72842ec36aae9c63fc874fc70
KhilfiKhairulAmin <khilfikhairulamin@gmail.com>
2024-04-20 23:09:55 +0800

    Add hello 2

--- db.py
+++ db.py
@@ -4,7 +4,7 @@ Author: Khilfi, Murfiqah, Zainatul
 Perform database operations for reading, and saving data from all text file.
 """
 print("Hello world!")
-
+print("Hello 2")
 def loadOrders():
   orders: list[dict] = []
   with open('db/orders.txt') as file:
